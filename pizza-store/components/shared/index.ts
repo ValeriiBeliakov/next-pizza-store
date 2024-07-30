8981f47c87d264729af167942ab0daa42ab0c46d
valeriy beliakov <valera08072006@mail.ru>
2024-07-30 20:02:54 +0300

    initial commit - was added header and filters

---
+++ pizza-store/components/shared/index.ts
@@ -0,0 +1,12 @@
+import { RangeSlider } from './range-slider';
+import { FilterCheckbox } from './filter-checkbox';
+import { Filters } from './filters';
+import { TopBar } from './top-bar';
+import { Categories } from "./categories";
+import { Container } from "./container";
+import { Header } from "./header";
+import { Title } from "./title";
+import { SortPopup } from "./sort-popup";
+
+
+export {Categories,SortPopup,Container,Header,Title,TopBar,Filters,FilterCheckbox,RangeSlider}
